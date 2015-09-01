e20068ffa963074008837324867c9dad9f0dffb8
Fort Dev <fortitude20@live.com>
2015-09-01 16:50:19 -0400

    Use get_absolute_url on List model to DRY urls in views. Something went wrong with routing, had to pull from source

--- lists/tests/test_models.py
+++ lists/tests/test_models.py
@@ -1,9 +1,8 @@
+from django.core.exceptions import ValidationError
 from django.test import TestCase
 from lists.models import Item, List
-from django.core.exceptions import ValidationError
 
-
-class ListAndItemModelTest(TestCase):
+class ListAndItemModelsTest(TestCase):
 
     def test_saving_and_retrieving_items(self):
         list_ = List()
@@ -32,9 +31,16 @@ class ListAndItemModelTest(TestCase):
         self.assertEqual(second_saved_item.text, 'Item the second')
         self.assertEqual(second_saved_item.list, list_)
 
+
+
     def test_cannot_save_empty_list_items(self):
         list_ = List.objects.create()
         item = Item(list=list_, text='')
         with self.assertRaises(ValidationError):
             item.save()
             item.full_clean()
+
+
+    def test_get_absolute_url(self):
+        list_ = List.objects.create()
+        self.assertEqual(list_.get_absolute_url(), '/lists/%d/' % (list_.id,))
