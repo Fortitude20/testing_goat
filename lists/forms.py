b7f605cdc5735e71f041e8ca6e956615654b1ad3
Fort Dev <fortitude20@live.com>
2015-09-02 13:25:21 -0400

    end of Chapter 11. Unit tests pass

--- lists/forms.py
+++ lists/forms.py
@@ -2,7 +2,6 @@ from django import forms
 
 from lists.models import Item
 
-
 EMPTY_ITEM_ERROR = "You can't have an empty list item"
 
 class ItemForm(forms.models.ModelForm):
@@ -19,3 +18,8 @@ class ItemForm(forms.models.ModelForm):
         error_messages = {
             'text': {'required': EMPTY_ITEM_ERROR}
         }
+
+
+    def save(self, for_list):
+        self.instance.list = for_list
+        return super().save()
