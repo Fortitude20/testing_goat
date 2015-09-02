45d47e60c65130c40e3787bc0b78ad3811e4fd09
Fort Dev <fortitude20@live.com>
2015-09-02 14:39:13 -0400

    End of chapter 12, forms validate_unique items in same list

--- lists/tests/test_forms.py
+++ lists/tests/test_forms.py
@@ -4,7 +4,6 @@ from lists.forms import (
     DUPLICATE_ITEM_ERROR, EMPTY_ITEM_ERROR,
     ExistingListItemForm, ItemForm
 )
-
 from lists.models import Item, List
 
 
@@ -31,6 +30,7 @@ class ItemFormTest(TestCase):
         self.assertEqual(new_item.list, list_)
 
 
+
 class ExistingListItemFormTest(TestCase):
 
     def test_form_renders_item_text_input(self):
@@ -52,3 +52,10 @@ class ExistingListItemFormTest(TestCase):
         form = ExistingListItemForm(for_list=list_, data={'text': 'no twins!'})
         self.assertFalse(form.is_valid())
         self.assertEqual(form.errors['text'], [DUPLICATE_ITEM_ERROR])
+
+
+    def test_form_save(self):
+        list_ = List.objects.create()
+        form = ExistingListItemForm(for_list=list_, data={'text': 'hi'})
+        new_item = form.save()
+        self.assertEqual(new_item, Item.objects.all()[0])
